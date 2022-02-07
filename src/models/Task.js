d0205c656c299a0ee71c03165e0e6f923184849b
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-07 01:30:11 -0400

    agregando el modelo Task con su respectivo schema

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,1 +1,20 @@
-import {Schema,model} from 'mongoose';
+import { Schema, model } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    //asi seria un esquema en mongo (un esquema son los datos que se guardaran)
+    title: {
+      type:String,
+      required:true,
+      unique:true,
+      trim:true  
+    },
+    description: String,
+    done: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Task", taskSchema);//asi se exporta un modelo para que sea requerido en el proyecto
